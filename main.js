7019d097b882a1f7e3f915ed08f941ad7a38de71
Sam1ndaFernando <uthpala2001@gmail.com>
2024-02-13 11:05:50 +0530

    completed

--- main.js
+++ main.js
@@ -5,8 +5,49 @@ const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
 
 function getRandomNumber(min, max) {
-    // Calculate the random number between min and max (inclusive)
+    // Calculate the random number between min and max
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
     return randomNumber;
 }
+
+btnNo.addEventListener("mouseover", (event) => {
+    const containerHeight = container.getBoundingClientRect().height;
+    const containerWidth = container.getBoundingClientRect().width;
+    const btnHeight = btnNo.getBoundingClientRect().height;
+    const btnWidth = btnNo.getBoundingClientRect().width;
+    const btnTop = btnNo.getBoundingClientRect().top;
+    const btnLeft = btnNo.getBoundingClientRect().left;
+
+    let newTop = btnTop;
+    let newLeft = btnLeft;
+    while (Math.abs(newTop - btnTop) < containerHeight / 3) {
+        newTop = getRandomNumber(0, containerHeight - btnHeight);
+    }
+    while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
+        newLeft = getRandomNumber(0, containerWidth - btnWidth);
+    }
+    btnNo.style.top = Math.floor(newTop) + "px";
+    btnNo.style.left = Math.floor(newLeft) + "px";
+});
+
+
+btnYes.addEventListener("click", (e) => {
+    btnNo.classList.add("hide");
+    imageOne.classList.add("hide");
+    imageTwo.classList.remove("hide");
+    
+    // Add the text "Hureee Huree" to the container
+    const hureeeText = document.createElement("p");
+    hureeeText.textContent = "Yhe Yhe Huree Huree!! My Crush said yessss !!!";
+    //hureeeText.textContent = "Yhe Yhe Huree Huree!! අපි room යමු !!!";
+    hureeeText.style.fontSize = "3rem";
+    hureeeText.style.color = "#F875AA"; 
+    hureeeText.style.fontFamily = "ABeeZee"; 
+    hureeeText.style.textAlign = "center"; 
+    container.appendChild(hureeeText);
+});
+
+
+
+
